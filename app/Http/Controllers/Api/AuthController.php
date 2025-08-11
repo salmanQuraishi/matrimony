148d7f86d2e2e63a1260d83a415bd1f38a87b33f
salman quraishi <sq4151760@gmail.com>
2025-08-11 16:38:39 +0530

    home api update3

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -438,9 +438,7 @@ class AuthController extends Controller
     public function Home()
     {
         try {
-            
             $user = auth()->user();
-
             if (!$user) {
                 return response()->json([
                     'status'  => false,
